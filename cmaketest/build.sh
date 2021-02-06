f48eb111f90903b8b0f1ab80c0d51c6538bc84c4
Shree Devi Kumar <5095331+Shreeshrii@users.noreply.github.com>
2021-02-06 02:03:23 +0000

    cmake Daily Action workflow - build and run basicapitest

--- cmaketest/build.sh
+++ cmaketest/build.sh
@@ -1,4 +1,6 @@
-### from root
+### only for reference - do not run
+
+### from root - build leptonica and tesseract from source
 
 git clone git@github.com:DanBloomberg/leptonica.git
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -S leptonica -B leptonica-build
@@ -13,3 +15,26 @@ cmake --install tesseract-build --prefix tesseract-install
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$(readlink -f tesseract-install) -DLeptonica_DIR=$(readlink -f leptonica-install/lib/cmake) -S cmaketest -B test-build
 cmake --build test-build
 cmake --install test-build --prefix test-install
+
+==========================================================
+# in tesseract directory - using preinstalled leptonica
+
+# build tesseract
+rm -rf build
+mkdir build && cd build
+cmake \
+-DOPENMP_BUILD=ON \
+-DCMAKE_INSTALL_PREFIX=inst \
+-DCMAKE_BUILD_TYPE=Release ..
+cd ..
+cmake --build build --config Release --target install
+
+# build basicapitest
+rm -rf test-build  
+cmake \
+-S cmaketest \
+-B test-build \
+-G Ninja \
+-DCMAKE_INSTALL_PREFIX:PATH=test-inst
+cmake --build test-build 
+test-build/basicapitest
